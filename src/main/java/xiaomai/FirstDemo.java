ea437f86f2633bcff8c619d3c890769389a30f4b
markwuwu <mark188988@163.com>
2019-05-12 11:23:54 +0800

    你是一个蠢货

---
+++ src/main/java/xiaomai/FirstDemo.java
@@ -0,0 +1,7 @@
+package xiaomai;
+
+public class FirstDemo {
+    public static void main(String[] args) {
+        System.out.println("你是一只猪");
+    }
+}
---
+++ src/main/java/xiaomai/SecondDemo.java
@@ -0,0 +1,7 @@
+package xiaomai;
+
+public class SecondDemo {
+    public static void main(String[] args) {
+        System.out.println("你是两只猪");
+    }
+}

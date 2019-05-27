f045838ef48956dbe84754210f825c8fc47e1a8b
markwuwu <mark188988@163.com>
2019-05-27 23:49:21 +0800

    11

--- src/main/java/xiaomai/SecondDemo.java
+++ src/main/java/xiaomai/SecondDemo.java
@@ -4,6 +4,7 @@ public class SecondDemo {
     public static void main(String[] args) {
         //好多好多244
         //好多好多ooo
+        System.out.println("两个人");
         System.out.println("你是两只猪");
     }
 }

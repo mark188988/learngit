26036ecfc8139c15e55c81886652594ba0ea5016
markwuwu <mark188988@163.com>
2019-05-28 00:21:51 +0800

    本地仓库提交哦

--- src/main/java/xiaomai/FirstDemo.java
+++ src/main/java/xiaomai/FirstDemo.java
@@ -3,6 +3,7 @@ package xiaomai;
 public class FirstDemo {
     public static void main(String[] args) {
         //修改了
+        //真的修改了
         System.out.println("你是一只猪");
     }
 }
---
+++ src/main/java/xiaomai/ThridDemo.java
@@ -0,0 +1,7 @@
+package xiaomai;
+
+public class ThridDemo {
+    public static void main(String[] args){
+        System.out.println("你");
+    }
+}
